7ed770de3de3bceec870770d69bc0e528c004a2e
Enrique Esquinas <esquinas.enrique@gmail.com>
2026-05-30 09:55:02 +0200

    Update rating.rb

--- lib/rating.rb
+++ lib/rating.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rating = Data.define(:score) do
-  NULL = new(score: nil).freeze
-
-  def self.null = NULL
+  def self.null = new(score: nil).freeze
   def formatted = score && format("★ %.1f", score)
 end
